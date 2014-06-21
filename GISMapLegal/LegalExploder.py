2f5f6b86c367ccd278dafbfbc3219df2bf6085d2
Sam Haycraft <phreakdawg@gmail.com>
2014-06-20 22:58:28 -0600

    i am drunk

---
+++ GISMapLegal/LegalExploder.py
@@ -0,0 +1,13 @@
+def explodetoarray(str_legal):
+    legalout = {}
+    legallist = str_legal.split(',')
+    print legallist
+    for i in range(0, len(legallist)):
+        print i
+
+
+def explode_single_array(s):
+
+
+
+explodetoarray("NW/4,S/2")
