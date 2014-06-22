6882d0550308cf558cba8e9b90d28d9f0ec80b32
Sam Haycraft <phreakdawg@gmail.com>
2014-06-21 22:35:40 -0600

    Have pushing divs and quarters stacks correctly

--- GISMapLegal/LegalExploder.py
+++ GISMapLegal/LegalExploder.py
@@ -9,18 +9,23 @@ def explodetoarray(str_legal):
 def explode_single_array(s):
     land_grid = {}  # this should be a 4x4 multi-array
     divs = []  # can use lists as stacks, see https://docs.python.org/2/tutorial/datastructures.html
-    quarters = {}
-    sb_legal_out = {}
+    quarters = []
+    sb_legal_out = []
 
     idx = 0
     for i in s:
-        print i
         if i == '/':
             divs.append(s[idx+1])
-            print s[idx+1]
-        idx = idx + 1
+
+            # half or quarter?
+            if divs[-1] == '2':
+                quarters.append(s[idx-1: idx+1])
+            elif divs[-1] == '4':
+                quarters.append(s[idx-2: idx+2])
+        idx += 1
 
     print divs
+    print quarters
 
 
-explode_single_array('NW/4')
+explode_single_array('N/2NW/4')
