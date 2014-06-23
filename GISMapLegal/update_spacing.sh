1f726cd59f785b1019ff8e39f3ce758da34ddde9
Sam Haycraft <phreakdawg@gmail.com>
2014-06-23 17:14:47 -0600

    pulling from cogcc website and correctly extracting tables

--- GISMapLegal/update_spacing.sh
+++ GISMapLegal/update_spacing.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export TMP=./tmp
+export OUT_FILE=./tmp/COGCC_spacing.csv
 
 if [ -d tmp ]; then
 	rm -Rf $TMP/*
@@ -10,5 +11,13 @@ fi
 
 wget --directory=$TMP http://cogcc.state.co.us/COGIS_Help/CauseOrderTable_Download.zip
 unzip -d $TMP $TMP/CauseOrderTable_Download.zip
-mdb-export $TMP/COGCC_OrdersTable_April_28_2014.mdb COGCC_Spacing_Download | sed -e 's/\s\+//g' > $TMP/shit.csv
-cat $TMP/shit.csv
+mdb-export $TMP/COGCC_OrdersTable_April_28_2014.mdb COGCC_Spacing_Download | sed -e 's/\s\+//g' > $OUT_FILE 
+
+sed -i -e 's/NW/NW\/4/g' $OUT_FILE
+sed -i -e 's/SW/SW\/4/g' $OUT_FILE
+sed -i -e 's/NE/NE\/4/g' $OUT_FILE
+sed -i -e 's/SE/SE\/4/g' $OUT_FILE
+sed -i -e 's/N2/N\/2/g' $OUT_FILE
+sed -i -e 's/S2/S\/2/g' $OUT_FILE
+sed -i -e 's/W2/W\/2/g' $OUT_FILE
+sed -i -e 's/E2/E\/2/g' $OUT_FILE
