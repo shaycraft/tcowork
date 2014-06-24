c1ee90538e1c00b1f46fb878f1895a9e4a64669f
shaycraft <phreakdawg@gmail.com>
2014-06-24 12:36:30 -0600

    finished

--- GISMapLegal/MapSpacing.py
+++ GISMapLegal/MapSpacing.py
@@ -2,7 +2,9 @@ __author__ = 'shaycraft'
 import csv
 from sys import argv
 import sys
+import re
 from LegalExploder import *
+from Lndkey import *
 
 if len(argv) != 2:
     sys.stderr.write('Usage: {0} <file>'.format(argv[0]))
@@ -12,7 +14,9 @@ if len(argv) != 2:
 # 8744,"18","8N","68W","6","E/2,SE/4SW/4","84","5","",2581,"JSND","http://cogcc.state.co.us/orders/orders/84/5.html"
 
 with open(argv[1]) as csvfile:
-    reader = csv.reader(csvfile, delimiter= ',', quotechar='"')
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    # skip header
+    next(reader, None)
 
     for row in reader:
         section_id = row[0]
@@ -21,6 +25,17 @@ with open(argv[1]) as csvfile:
         formation = row[10]
         link = row[11]
 
+        # lndkey
+        section = int(row[1])
+        townregex = re.match('(\d+)([S|N|E|W])', row[2])
+        township = int(townregex.group(1))
+        tdir = townregex.group(2)
+        rangeregex = re.match('(\d+)([S|N|E|W])', row[3])
+        rnge = int(rangeregex.group(1))
+        rdir = rangeregex.group(2)
+        meridian = int(row[4])
+
         legal_exploded = explode(row[5])
         for x in legal_exploded:
+            lkey = generate_lndkey(township, tdir, rnge, rdir, meridian, section, x, 'CO')
             print '{0},{1},{2},{3},"{4}",{5}'.format(section_id, legal, unit_size, formation, link, x)
