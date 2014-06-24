121d045dab6d0bcec470ff7ac1efbf64d0d57f49
Sam Haycraft <phreakdawg@gmail.com>
2014-06-24 15:33:24 -0600

    aliasing geometry column

--- GISMapLegal/joinquery.sql
+++ GISMapLegal/joinquery.sql
@@ -1,1 +1,1 @@
-select max(s.id) as id, max(s.unit_size) as unit_size, s.formation, s.lkey, st_asewkt(st_union(q.geom)) from staging.spacing s inner join plss.qq q on q.secdivid=s.lkey group by s.formation, s.lkey;
+select max(s.id) as id, max(s.unit_size) as unit_size, s.formation, s.lkey, st_asewkt(st_union(q.geom)) as geom from staging.spacing s inner join plss.qq q on q.secdivid=s.lkey group by s.formation, s.lkey;
