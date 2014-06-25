7c430b5123927793614a20079ff984dcee5cdd26
Sam Haycraft <phreakdawg@gmail.com>
2014-06-25 09:46:13 -0600

    finished

--- GISMapLegal/update_spacing.sh
+++ GISMapLegal/update_spacing.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+read -s -p "Enter db password for $USER: " pgpass
+export PGPASSWORD=$pgpass
+
+cd /home/shaycraft/src/github-shaycraft/tcowork/GISMapLegal
+
 export TMP=./tmp
 export OUT_FILE=./tmp/COGCC_spacing.csv
 
@@ -21,3 +26,14 @@ sed -i -e 's/N2/N\/2/g' $OUT_FILE
 sed -i -e 's/S2/S\/2/g' $OUT_FILE
 sed -i -e 's/W2/W\/2/g' $OUT_FILE
 sed -i -e 's/E2/E\/2/g' $OUT_FILE
+
+python ./MapSpacing.py $OUT_FILE > $TMP/spacing_exploded.csv
+
+psql -d cogcc -c "DELETE FROM staging.spacing"
+psql -d cogcc -c "\copy staging.spacing(section_id,legal,unit_size,formation,link,lkey) FROM '$TMP/spacing_exploded.csv' WITH DELIMITER AS ',' CSV HEADER;"
+psql -d cogcc -c "vacuum analyze staging.spacing"
+
+pgsql2shp -f $TMP/co_spacing.shp -P samcool -g geom cogcc "`cat joinquery.sql`"
+zip $TMP/co_spacing.zip $TMP/co_spacing.*
+
+echo "Please see attached file" | mail -s "COGCC spacing file" -a $TMP/co_spacing.zip "`whoami`@tcolandservices.com"
